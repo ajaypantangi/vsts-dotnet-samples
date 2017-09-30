bc3c4a015d1b1d03b1e1e1a9a6732503e79a3939
Ajay Pantangi <ajaypa@microsoft.com>
2017-09-30 00:56:28 -0700

    board apis samples

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Work/BoardParentsSample.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi.Types;
+using Microsoft.TeamFoundation.Work.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.TeamServices.Samples.Client.Work
+{
+    [ClientSample(WorkWebConstants.RestArea, "boardparents")]
+    public class BoardParentsSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public IEnumerable<ParentChildWIMap> GetBoardMappingParentItems()
+        {
+            VssConnection connection = Context.Connection;
+            WorkHttpClient workClient = connection.GetClient<WorkHttpClient>();
+
+            Guid projectId = ClientSampleHelpers.FindAnyProject(this.Context).Id;
+            Guid teamId = ClientSampleHelpers.FindAnyTeam(this.Context, projectId).Id;
+            var context = new TeamContext(projectId, teamId);
+
+            string childBacklogContextCategoryRefName = "Microsoft.RequirementCategory";
+            List<int> workItemIds = new List<int>() { 5, 6, 7, 8};
+
+            IEnumerable<ParentChildWIMap> result = workClient.GetBoardMappingParentItemsAsync(
+                context, 
+                childBacklogContextCategoryRefName, 
+                workItemIds).Result;
+
+            Console.WriteLine("{0}'s auto refresh state: {1}");
+
+            return result;
+        }
+    }
+}
---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Work/BoardUserSettingsSample.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi.Types;
+using Microsoft.TeamFoundation.Work.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.TeamServices.Samples.Client.Work
+{
+    [ClientSample(WorkWebConstants.RestArea, "boardusersettings")]
+    public class BoardUserSettingsSample : ClientSample
+    {
+
+        [ClientSampleMethod]
+        public BoardUserSettings GetBoardUserSettings()
+        {
+            VssConnection connection = Context.Connection;
+            WorkHttpClient workClient = connection.GetClient<WorkHttpClient>();
+
+            Guid projectId = ClientSampleHelpers.FindAnyProject(this.Context).Id;
+            Guid teamId = ClientSampleHelpers.FindAnyTeam(this.Context, projectId).Id;
+            var context = new TeamContext(projectId, teamId);
+
+            List<BoardReference> allBoards = workClient.GetBoardsAsync(context).Result;
+            BoardReference board = allBoards.FirstOrDefault();
+            BoardUserSettings result = workClient.GetBoardUserSettingsAsync(context, board.Id.ToString()).Result;
+
+            Console.WriteLine("{0}'s auto refresh state: {1}", board.Name, result.AutoRefreshState);
+
+            return result;
+        }
+
+        [ClientSampleMethod]
+        public BoardUserSettings UpdateBoardUserSettings()
+        {
+            BoardUserSettings boardUserSettings = new BoardUserSettings() {
+                AutoRefreshState = true
+            };
+            Dictionary<string, string> settings = new Dictionary<string, string>() { };
+            settings.Add("AutoRefreshState", true.ToString());
+
+            VssConnection connection = Context.Connection;
+            WorkHttpClient workClient = connection.GetClient<WorkHttpClient>();
+
+            Guid projectId = ClientSampleHelpers.FindAnyProject(this.Context).Id;
+            var context = new TeamContext(projectId);
+            List<BoardReference> allBoards = workClient.GetBoardsAsync(context).Result;
+            BoardReference board = allBoards.FirstOrDefault();
+
+            var result = workClient.UpdateBoardUserSettingsAsync(settings, context, board.Id.ToString()).Result;
+
+            Console.WriteLine("{0}'s auto refresh state: {1}", board.Name, result.AutoRefreshState);
+
+            return result;
+        }
+    }
+}
